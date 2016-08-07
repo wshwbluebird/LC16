a7d170467a7aa725dcbfc3e0bc22b97f2ac83cc6
wshwbluebird <wshwbluebird@outlook.com>
2016-08-07 21:35:33 +0800

    20160807

---
+++ 10PalindromeNumber.cpp
@@ -0,0 +1,22 @@
+//
+// Created by wshwbluebird on 16/8/7.
+//
+
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+         const char *x = p.c_str();
+        cout<<(x+2)<<endl;
+
+    }
+};
+
+
+int main(){
+    Solution s;
+    s.isMatch("sdf","DSfsdfsdf");
+
+    return 0;
+}
